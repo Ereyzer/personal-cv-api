7aefcc9585506450c1a0e05a7cb78fefda607619
Ivan <ereyzer142@gmail.com>
2025-07-31 21:25:11 +0300

    test create file

--- app/src/utils/createDirIfNotExist.ts
+++ app/src/utils/createDirIfNotExist.ts
@@ -9,6 +9,8 @@ export const createDiirIfNotExist = async (tmpPathString: string) => {
     await fs.writeFile(tmpPathString + '/test.txt', 'Hello World');
     console.log(await fs.readFile(tmpPathString + '/test.txt', { encoding: 'utf-8' }));
   } catch (error) {
+    console.log(error);
+
     if (error instanceof Error && (error as { code?: string }).code === 'ENOENT')
       await fs.mkdir(tmpPathString);
     console.log('create dir');
