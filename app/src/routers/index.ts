ea25ffff4edc33967c3bfca67e54310139908e4c
Ivan <ereyzer142@gmail.com>
2025-08-02 20:05:28 +0300

    cors to routes

--- app/src/routers/index.ts
+++ app/src/routers/index.ts
@@ -2,11 +2,13 @@ import express, { Router } from 'express';
 import AdminRouter from './admin.ts';
 import authRouter from './auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.ts';
+import { corsCallBAck } from '../utils/corsCAllback.ts';
+import cors from 'cors';
 
 const router = Router();
 
 router.use('/', ctrlWrapper(express.static('/static')));
-router.use('/admin', AdminRouter);
-router.use('/auth', authRouter);
+router.use('/admin', cors(corsCallBAck), AdminRouter);
+router.use('/auth', cors(corsCallBAck), authRouter);
 
 export default router;
